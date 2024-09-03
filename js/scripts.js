4032d6e6e5e87a131083d3b212cbaf4be513a5fd
Sydney <sydney.fryer@ai.metagon.com>
2024-09-03 22:40:08 +0200

    Messaging

--- js/scripts.js
+++ js/scripts.js
@@ -54,3 +54,33 @@ function toggleDescription(id, showLinkId) {
     if (showLink) showLink.style.display = "block";
   }
 }
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent the default form submission
+
+  const form = event.target;
+  const formData = new FormData(form);
+
+  fetch(form.action, {
+      method: form.method,
+      body: formData,
+      headers: {
+          'Accept': 'application/json'
+      }
+  }).then(response => {
+      if (response.ok) {
+          document.getElementById('confirmationMessage').style.display = 'block';
+          form.reset(); // Optionally reset the form
+      } else {
+          return response.json().then(data => {
+              if (Object.hasOwn(data, 'errors')) {
+                  alert(data["errors"].map(error => error["message"]).join(", "));
+              } else {
+                  alert("Oops! There was a problem submitting your form");
+              }
+          });
+      }
+  }).catch(error => {
+      alert("Oops! There was a problem submitting your form");
+  });
+});
